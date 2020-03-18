3f05e16422056027739de46b09b222ca64510ef9
zhangxintaobb <1309526743@qq.com>
2020-03-18 11:42:11 +0800

    5

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,20 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+            /* 压缩图片大小 */
+            dist: {
+                options: {
+                    optimizationLevel: 1 //定义 PNG 图片优化水平
+                },
+                files: [{
+                    expand: true,
+                    cwd: './img', //原图存放的文件夹
+                    src: ['**/*.{png,jpg,jpeg,gif}'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
+                    dest: 'dist/img/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+                }]
+            }
+        },
         copy: {
             html: {
                 src: "./demo.html",
@@ -32,6 +46,10 @@ module.exports = function(grunt) {
             js: {
                 src: "./demo.js",
                 dest: "./dist/demo.js"
+            },
+            image: {
+                src: "./img",
+                dest: "./dist/img"
             }
         }
     })
@@ -41,6 +59,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
     grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask("release", ['copy', 'htmlmin', 'cssmin', 'uglify']);
+    grunt.registerTask("release", ['copy', 'htmlmin', 'cssmin', 'uglify', 'imagemin']);
 }
